a3f2d4edc8f758be6042db997db8b4485d709901
Roi Attia <roiattia.1@gmail.com>
2018-05-11 16:15:03 +0300

    Added Ad

--- app/src/main/java/roiattia/com/thatjoke/MainActivity.java
+++ app/src/main/java/roiattia/com/thatjoke/MainActivity.java
@@ -3,30 +3,73 @@ package roiattia.com.thatjoke;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 import roiattia.com.jokelibrary.JokeActivity;
 import roiattia.com.joker.Joker;
 
 public class MainActivity extends AppCompatActivity {
 
+    private InterstitialAd mInterstitialAd;
+    private Joker mJoker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Joker joker = new Joker();
+        mJoker = new Joker();
+        final boolean isFreeFlavor = BuildConfig.IS_FREE;
+
+        if (BuildConfig.IS_FREE) {
+            initializeAd();
+        } else {
+            Toast.makeText(this, "paid flavor", Toast.LENGTH_SHORT).show();
+        }
 
         Button button = findViewById(R.id.btn_joke);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, JokeActivity.class);
-                intent.putExtra("joke", joker.getJoke());
-                startActivity(intent);
-//                Toast.makeText(MainActivity.this, joker.getJoke(), Toast.LENGTH_SHORT).show();
+                if(isFreeFlavor) {
+                    if (mInterstitialAd.isLoaded()) {
+                        mInterstitialAd.show();
+                    } else {
+                        Log.d("TAG", "The interstitial wasn't loaded yet.");
+                    }
+                } else {
+                    openJokeActivity();
+                }
+            }
+        });
+    }
+
+    private void openJokeActivity() {
+        Intent intent = new Intent(MainActivity.this, JokeActivity.class);
+        intent.putExtra("joke", mJoker.getJoke());
+        startActivity(intent);
+    }
+
+    private void initializeAd() {
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+                openJokeActivity();
             }
         });
     }
