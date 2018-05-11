6239592ec49c0ac3fd9ea123dc21e887bde4ee38
Roi Attia <roiattia.1@gmail.com>
2018-05-11 15:34:15 +0300

    Added Java library and flavors

--- app/src/main/java/roiattia/com/thatjoke/MainActivity.java
+++ app/src/main/java/roiattia/com/thatjoke/MainActivity.java
@@ -2,6 +2,11 @@ package roiattia.com.thatjoke;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import roiattia.com.joker.Joker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Joker joker = new Joker();
+
+        Button button = findViewById(R.id.btn_joke);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, joker.getJoke(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ joker/src/main/java/roiattia/com/joker/Joker.java
@@ -0,0 +1,8 @@
+package roiattia.com.joker;
+
+public class Joker {
+
+    public String getJoke(){
+        return "This is a joke from the Joker";
+    }
+}
